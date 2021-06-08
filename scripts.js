cfbb6b124d5acb02f758aef77857f25224949f4a
jordan-hess <82159695+jordan-hess@users.noreply.github.com>
2021-06-08 22:43:47 +0200

    update

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger');
+const navbarlinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.navbarlink');
+
+
+burger.addEventListener('click', () =>{
+    
+});
